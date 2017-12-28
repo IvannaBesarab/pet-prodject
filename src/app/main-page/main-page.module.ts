eaaa91aa7dd9c9048bc88b3327f93a18e70f2186
Ivanna Besarab <ivanna@besarab.com>
2017-12-28 22:48:39 +0200

    Added basic card for Login page

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -1,10 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page.component';
 import { HeaderComponent } from './header/header.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
